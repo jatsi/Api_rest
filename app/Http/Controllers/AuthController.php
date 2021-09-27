690313b83f9d5840212abc1d74972da88d33c013
jatsi <33884168+jatsi@users.noreply.github.com>
2021-09-27 18:21:18 -0500

    signin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         ], 201);
     }
 
-    public function login(Request $request){
+    public function signin(Request $request){
 
         $request->validate([
             'email' => 'required|string|email',
