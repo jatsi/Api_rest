b5a0b857988b2fc81524da63ac0c5f0b9e560901
jatsi <33884168+jatsi@users.noreply.github.com>
2021-09-27 14:44:20 -0500

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class AuthController extends Controller
+{
+    public function signup(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'status' => 'ok'
+        ], 201);
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+           
+        ]);
+        $credentials = request(['email', 'password']);
+        if(!Auth::attempt($credentials))
+            return response()->json([
+                'status' => 'error',
+                'result' =>  'Datos Invalidos'
+            ]);
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        $token->save();
+        return response()->json([
+            'status' => 'ok',
+            'token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+
+    public function logout(Request $request){
+        $request->user()->token()->revoke();
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    }
+
+    public function user(Request $request){
+        return response()->json($request->user());
+    }
+}
